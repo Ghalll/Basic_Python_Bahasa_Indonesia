3904ab1e62d598b0a8fe0a1df433f37351bed381
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-11 10:16:09 +0700

    Add files via upload

---
+++ Sub Program/Prosedur.py
@@ -0,0 +1,17 @@
+# PROSEDUR 
+"""
+Adalah fungsi yang tidak menghasilkan nilai. 
+Prosedur tidak mengharuskan adanya parameter input atau output
+Prosedur didefinisikan dengan return tanpa ekspresi atau nilai yang dihasilkan di akhir fungsi.
+"""
+
+# Syntax dasar prosedur pada Python.
+def sapa(nama):
+    print(f"Halo, {nama}!")
+# Memanggil prosedur
+sapa("Ayu") 
+
+# Contoh lain
+def greeting(name):
+    print("Halo " + name + ", Selamat Datang")
+greeting("Ghaly")
