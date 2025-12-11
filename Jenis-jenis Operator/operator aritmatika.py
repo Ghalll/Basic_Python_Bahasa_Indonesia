87f356caa020d1728c8c15f19a76afb0e7225c17
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-11 10:05:35 +0700

    Add files via upload

---
+++ Jenis-jenis Operator/operator aritmatika.py
@@ -0,0 +1,25 @@
+# OPERATOR ARITMATIKA
+# Jenis pertama adalah operator untuk melakukan operasi aritmetika.
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+"""
+▶️ Penjumlahan `(+)` : Menambahkan nilai dari kedua operan. => x + y = 16
+▶️ Pengurangan `(-)` : Mengurangi nilai dari kedua operan. => x - y = 6
+▶️ Perkalian `(*)`   : Mengalikan nilai dari kedua operan. => x * y = 55
+▶️ Pembagian Bulat `(//)` : Membagi nilai dari kedua operan. Jika operan adalah integer, hasil operasi adalah bilangan bulat. => x // y = 2
+▶️ Pembagian Riil `(/)` : Membagi nilai dari kedua operan. Jika operan adalah float, hasil operasi adalah bilangan riil. => x / y = 2.2
+▶️ Modulo `(%)` : Sisa hasil pembagian nilai dari dua operan. => x % y = 1
+▶️ Pangkat `(**)` : Memangkatkan nilai dari dua operan. => x ** y = 161051
+"""
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+
+x = 11
+y = 5
+
+print(x + y)    # 16
+print(x - y)    # 6
+print(x * y)    # 55
+print(x // y)   # 2
+print(x / y)    # 2.2
+print(x % y)    # 1
+print(x ** y)   # 161051
+
