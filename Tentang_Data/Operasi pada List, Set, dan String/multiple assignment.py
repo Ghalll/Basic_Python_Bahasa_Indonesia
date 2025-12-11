053de46184449599ecdc089d9fd06b17f3b11295
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-11 09:58:11 +0700

    Add files via upload

---
+++ Tentang_Data/Operasi pada List, Set, dan String/multiple assignment.py
@@ -0,0 +1,12 @@
+# Multiple assignment adalah fitur yang memungkinkan Anda memberikan nilai untuk beberapa variabel dalam satu operasi.
+# Jumlah variabel yang dimasukkan haruslah sama dengan jumlah variabel yang ada pada list atau tuple.
+
+data = ['Apple','MacBook retina 2015','ram8','ssd510','intel core i5']
+brand, merk, ram, penyimpanan, prosesor = data
+
+print(data)
+print(brand)
+print(merk)
+print(ram)
+print(penyimpanan)
+print(prosesor)
