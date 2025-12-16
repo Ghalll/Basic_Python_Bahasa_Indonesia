e92797095a43584fa812937600c65fb5bc17b733
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:05:22 +0700

    Create 03_Operator_Assigment.py

---
+++ 02_Jenis_Jenis_Operator/03_Operator_Assigment.py
@@ -0,0 +1,37 @@
+# OPERATOR ASSIGNMENT
+# Operator ini bertujuan untuk melakukan proses assignment atau pemberian nilai pada suatu variabel dengan nilai tetap.
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+"""
+▶️ Penambahan dan Pengisian (`+=`): Menyederhanakan operasi x = x + y.
+▶️ Pengurangan dan Pengisian (`-=`): Menyederhanakan operasi x = x - y.
+▶️ Perkalian dan Pengisian (`*=`): Menyederhanakan operasi x = x * y.
+▶️ Pembagian dan Pengisian (`/=`): Menyederhanakan operasi x = x / y.
+▶️ Modulus dan Pengisian (`%=`): 	Menyederhanakan operasi x = x % y.
+"""
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+
+# Penambahan dan Pengisian
+x = 5
+x += 3  # Sama dengan x = x + 3
+print(x)
+
+# Pengurangan dan Pengisian
+y = 10
+y -= 4  # Sama dengan y = y - 4
+print(y)
+
+# Perkalian dan Pengisian
+z = 3
+z *= 2  # Sama dengan z = z * 2
+print(z)
+
+# Pembagian dan Pengisian
+w = 8
+w /= 4  # Sama dengan w = w / 4
+print(w)
+
+# Modulus dan Pengisian
+a = 10
+b = 3
+a %= b  # Sama dengan a = a % b
+print(a)
