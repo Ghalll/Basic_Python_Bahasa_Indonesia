46ae8ab3c5a71b218eedbc08dfcd02675a996408
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:04:51 +0700

    Create 02_Operator_Logika.py

---
+++ 02_Jenis_Jenis_Operator/02_Operator_Logika.py
@@ -0,0 +1,32 @@
+# OPERATOR LOGIKA
+"""
+Operator logika merupakan jenis operator untuk melakukan operasi logika dengan kedua operannya bertipe boolean.
+Hasil akhir dari operasi ini adalah nilai bertipe boolean.
+"""
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+"""
+▶️ "AND" atau "&" : Logika yang hanya menghasilkan True jika kedua operan bernilai True.
+▶️ "OR" atau "|"  : Logika yang menghasilkan True jika salah satu dari kedua operan bernilai True. 
+▶️  NOT    : Logika yang bertujuan untuk membalikkan nilai logika dari operannya.
+"""
+
+# Operator `and`
+a = True
+b = False
+result = a and b
+print(result)  # Output: False
+
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+
+# Operator `or`
+a = True
+b = False
+result = a or b
+print(result)  # Output: True
+
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+
+# Operator `not`
+a = True
+result = not a
+print(result)  # Output: False
