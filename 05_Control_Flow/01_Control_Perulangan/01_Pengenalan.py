d0c6ca28867d3e9f5394bec103dfa4e9ec67c649
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:14:15 +0700

    Update 01_Pengenalan.py

--- 05_Control_Flow/01_Control_Perulangan/01_Pengenalan.py
+++ 05_Control_Flow/01_Control_Perulangan/01_Pengenalan.py
@@ -1,1 +1,10 @@
+# Selain membuat perulangan, kita juga dapat mengontrol perulangan dengan menggunakan beberapa pernyataan di antaranya sebagai berikut.
+# Kontrol Perulangan berfungsi untuk memanipulasi alur eksekusi perulangan
+"""
+1). `Break` => pernyataan untuk menghentikan perulangan secara paksa, bahkan jika kondisi perulangan masih benar.
+2). `continue` => digunakan untuk melanjutkan ke iterasi berikutnya, mengabaikan bagian dari blok perulangan yang dibawah.
+3). `Else for` => sebagai memberikan jalan keluar program saat pencarian tidak ditemukan. 
+4). `Else while` => blok statement else akan selalu dieksekusi saat kondisi pada while menjadi salah.
+5). `pass` => Digunakan sebagai placeholder untuk membiarkan blok perulangan tetap kosong.
 
+"""
