854fea16389dc896539b5f83a682243604e38895
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:23:47 +0700

    Delete Control Flow directory

--- Control Flow/nested loop.py
+++
@@ -1,9 +0,0 @@
-# PERULANGAN FOR YANG BERSARANG 
-#
-
-for i in range(5):
-  for j in range(i + 1):
-    print("*", end=" ")
-  print()
-
-
--- Control Flow/pengenalan.py
+++
@@ -1,9 +0,0 @@
-# CONTROL FLOW
-""" 
-👇👇👇👇👇👇
-➡️ adalah sebuah cara untuk memberi tahu program mengenai instruksi yang harus dijalankan dan di mana harus memulai dan berakhir.
-
-  Python memiliki beberapa struktur kontrol yang umum digunakan, Seperti `if`,`else`, dan `elif` untuk percabangan kondisional
-  dan untuk struktur kontrol perulangan, seperti `for` dan `while`
-"""
-
