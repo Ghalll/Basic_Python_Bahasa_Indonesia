c95fd91820b65e5d49dc85d631944a5a1ff475c0
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:02:49 +0700

    Delete Tentang_Data directory

--- Tentang_Data/Operasi pada List, Set, dan String/fungsi len.py
+++
@@ -1,7 +0,0 @@
-# Fungsi len() bertujuan untuk menghitung panjang atau banyaknya elemen dari list, set, dan string.
-
-contoh_list = [1, 3, 3, 5, 5, 5, 7, 7, 9]
-print(contoh_list)
-print(len(contoh_list))
-
-# output : 9
