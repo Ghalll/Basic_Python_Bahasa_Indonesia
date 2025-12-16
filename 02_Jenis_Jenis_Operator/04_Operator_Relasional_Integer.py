f7149d7309e02ee4e9429597b1a13b572d05a9ab
Ghaly risyadi <126685634+Ghalll@users.noreply.github.com>
2025-12-16 10:05:56 +0700

    Create 04_Operator_Relasional_Integer.py

---
+++ 02_Jenis_Jenis_Operator/04_Operator_Relasional_Integer.py
@@ -0,0 +1,25 @@
+# OPERATOR RELASIONAL PADA TIPE DATA NUMERIC (intger,float,string,boolean)
+"""
+Operator relasional merupakan operator perbandingan antara dua operan yang berupa integer, float, string, ataupun boolean.
+Hasil akhir dari operator ini adalah nilai bertipe boolean.
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+"""
+"""
+▶️ Sama dengan `(==)` : Menghasilkan True, jika kedua operan bernilai sama. 
+▶️ Tidak Sama dengan `(!=)` : Menghasilkan True, jika kedua operan tidak bernilai sama.
+▶️ Lebih Besar dari `(>)` : Menghasilkan True, jika operan kiri lebih besar dari operan kanan.
+▶️ Kurang dari `(<)` : Menghasilkan True, jika operan kanan lebih besar dari operan kiri. 
+▶️ Lebih Besar dari Sama dengan `(>=)` : Menghasilkan True, jika operan kiri lebih besar atau sama dengan operan kanan.
+▶️ Kurang dari Sama dengan `(<=)` : Menghasilkan True, jika operan kanan lebih besar atau sama dengan operan kiri.
+"""
+# ⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+
+x = 5
+y = 10
+
+print(x == y)   # false
+print(x != y)   # true
+print(x > y)    # false
+print(x < y)    # true
+print(x >= y)   # false
+print(x <= y)   # true
